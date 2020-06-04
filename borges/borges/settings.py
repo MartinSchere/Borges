bf0f6619c9a0119a79c89a0c1e32021040cd8dad
Martín Schere <tinschere@gmail.com>
2020-06-03 22:57:47 -0300

    mycommit

--- borges/borges/settings.py
+++ borges/borges/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1b#l^_8f3-c-pd@u)j6sl6%k$hukdli2-9ny246ogc3*s_7x!0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['borges1885.herokuapp.com']
 
 
 # Application definition
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     # user apps
     'main',
     'rest_framework',
-    'frontend',
     'graphene_django',
     'corsheaders',
     'crispy_forms',
@@ -49,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
 
 GRAPHENE = {
     'SCHEMA': 'main.schema.schema'
